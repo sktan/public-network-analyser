b38c7ada6f762dc66c487cbca179becf5e4e7f2a
Steven <contact@sktan.com>
2017-05-12 23:03:01 +1000

    Added methods to resolve external IP

--- libs/netrecon.py
+++ libs/netrecon.py
@@ -1,15 +1,29 @@
 """ Class for network functions """
 import socket
+# pip install dnspython
+import dns.resolver
 
 class NetRecon:
     """ Class for network functions """
     @staticmethod
     def out_interface():
         """ Gets the network IP address that has a route out """
+        # http://stackoverflow.com/a/166589
+        # Connects to an internet resource (google's DNS)
+        # and then returns the IP address that was used to connect
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.connect(("8.8.8.8", 80))
         return sock.getsockname()[0]
     @staticmethod
     def ping_subnet():
         """ Pings an entire subnet of IP addresses """
-        
+    @staticmethod
+    def get_external_ip():
+        """ Grabs the external IP address """
+        # We will use the OpenDNS DNS server to grab our public IP
+        my_resolver = dns.resolver.Resolver()
+        my_resolver.nameservers = ['208.67.222.222']
+        answer = my_resolver.query('myip.opendns.com.')
+        # HTTP fallback on Akamai's CDN
+        # http://whatismyip.akamai.com/.
+        return answer[0].address
--- recon.py
+++ recon.py
@@ -2,24 +2,26 @@
 import sys
 import platform
 from libs.netrecon import NetRecon
+# pip install pyspeedtest
 import pyspeedtest
 
 def print_os():
     """ Prints the OS to the console """
     print("Current Operating System: {} {}".format(platform.system(), platform.release()))
 
-def network_interfaces():
+def network_interface():
     """ Grabs the network interface information """
     net_out_ip = NetRecon.out_interface()
-    print(net_out_ip)
+    print("LAN Address: {0}".format(net_out_ip))
+    print("External IP: {0}".format(NetRecon.get_external_ip()))
     return None
 
 def speedtest():
     """ Runs a internet speedtest """
     net_speed = pyspeedtest.SpeedTest()
-    print(int(net_speed.ping()))
-    print(format_network_speed(net_speed.download()))
-    print(format_network_speed(net_speed.upload()))
+    print("Ping: {0}".format(int(net_speed.ping())))
+    print("Download Speed: {0}".format(format_network_speed(net_speed.download())))
+    print("Upload Speed: {0}".format(format_network_speed(net_speed.upload())))
 
 def format_network_speed(raw_bps=0):
     """ Formats a network speed test to human readable format """
@@ -29,7 +31,7 @@ def format_network_speed(raw_bps=0):
     while speed > 1024:
         index += 1
         speed /= 1024
-    return "%0.2f %s" % (int(speed), fmt[index])
+    return "%0.2f %s" % (speed, fmt[index])
 
 def main(args=None):
     """ The entry point of our script."""
@@ -37,7 +39,7 @@ def main(args=None):
         args = sys.argv[1:]
 
     print_os()
-    network_interfaces()
+    network_interface()
     speedtest()
 
 if __name__ == "__main__":
